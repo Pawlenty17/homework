9d97c9624f523d6ba230bfd11c2b416be5d4d9f3
Pavel <fedyachenko04@gmail.com>
2024-10-24 18:11:02 +0300

    Add hw_task3

---
+++ hw/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+int[] arr = { 1, 2, 3, 4, 5 };
+PrintArrayReverse(arr, arr.Length - 1);
+
+
+void PrintArrayReverse(int[] arr, int index)
+{
+    if (index < 0)
+        return;
+
+    Console.Write(arr[index]);
+    PrintArrayReverse(arr, index - 1);
+}
+    
